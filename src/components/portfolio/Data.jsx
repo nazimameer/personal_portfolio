315b891b365edc0357114b076d1c3140515cc381
Nazim Ameer <nazimameerpp@gmail.com>
2023-07-04 00:14:26 +0530

    share Prompt added

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -1,17 +1,26 @@
-import work1 from "../../assets/controlhub.png";
-import work2 from "../../assets/thetosh.png";
+import work3 from "../../assets/thetosh.png";
+import work1 from '../../assets/shareprompt.png'
+import work2 from "../../assets/controlhub.png";
 
 export const projectsData = [
+
   {
     id: 1,
     image: work1,
+    title: "Share Prompts",
+    subtitle: "AI-Powed Prompts",
+    link:'https://nazim-shareprompt.vercel.app/'
+  },
+  {
+    id: 2,
+    image: work2,
     title: "ControlHub",
     subtitle: "SaaS for HR Management",
     link:'https://controlhub.online/'
   },
   {
-    id: 2,
-    image: work2,
+    id: 3,
+    image: work3,
     title: "The Tosh",
     subtitle: "E-commerce Application",
     link:'https://www.thetosh.store/'
